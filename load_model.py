5873bed02c5d7427a5da5643e4faddc2207a074b
van huu <huunguyen0802@gmail.com>
2026-06-05 09:04:01 +0700

    nop

--- load_model.py
+++ load_model.py
@@ -10,7 +10,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
-gemini = genai.GenerativeModel("gemini-1.5-flash")
+gemini = genai.GenerativeModel("models/gemini-1.5-flash")
 
 app = FastAPI()
 
